8795d51e7397e907160f2a9421a902e7b61fe191
Arfat-sharda <2022505539.arfat@ug.sharda.ac.in>
2024-12-27 07:48:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Surprise Button Click Action
+document.getElementById("fun-button").addEventListener("click", function() {
+    alert("Surprise! You've just unlocked the madness of Certified Chutiye! 🚀");
+  });
+  
